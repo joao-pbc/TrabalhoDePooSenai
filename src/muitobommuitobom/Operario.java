64c7545b5e1a5479619eb6b554f855ce42ead884
CHRISTIAN PEIXER SARDO <christian_sardo@estudante.sesisenai.org.br>
2024-06-24 21:10:10 -0300

    Metodos Operário pt1

--- src/muitobommuitobom/Operario.java
+++ src/muitobommuitobom/Operario.java
@@ -8,7 +8,26 @@ public class Operario extends Empregado{
 
     public Operario(String nome, muitobommuitobom.TipoPessoa tipoPessoa, String documento, String endereco, String complementoEndereco, String bairro, String CEP, String cidade, muitobommuitobom.Estado estado, String telefone) {
         super(nome, tipoPessoa, documento, endereco, complementoEndereco, bairro, CEP, cidade, estado, telefone);
+        this.informouProducaoMensal = false;
+        this.producaoMensal = 0;
+        this.percentualComissao = percentualComissao;
+        this.valorMinimoGatilho = valorMinimoGatilho;
+
     }
+
+
+    public void InformarProduçãoMensal(double producaoMensal){
+        this.producaoMensal = producaoMensal;
+        this.informouProducaoMensal = true;
+
+    }
+
+    public void LimparProducaoMensal(){
+        this.producaoMensal = 0;
+        this.informouProducaoMensal = false;
+    }
+
+
 
     void informarProducaoMensal(double valor){
 
