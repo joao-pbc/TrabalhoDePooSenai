e9500cfc105ca0cdb141730d26771b804132c163
CHRISTIAN PEIXER SARDO <christian_sardo@estudante.sesisenai.org.br>
2024-06-24 21:50:00 -0300

    Classe empregado do cris

--- src/muitobommuitobom/Empregado.java
+++ src/muitobommuitobom/Empregado.java
@@ -14,9 +14,24 @@ public abstract class Empregado extends Pessoa{
 
     public Empregado(String nome, muitobommuitobom.TipoPessoa tipoPessoa, String documento, String endereco, String complementoEndereco, String bairro, String CEP, String cidade, muitobommuitobom.Estado estado, String telefone) {
         super(nome, tipoPessoa, documento, endereco, complementoEndereco, bairro, CEP, cidade, estado, telefone);
+        if(codigoSetor<0){
+            throw new IllegalArgumentException("O valor deve ser acima de zero");
+        }
+        if (salarioBruto > 0 && Math.floor(salarioBruto * 100) == salarioBruto * 100) {
+            throw new IllegalArgumentException("O valor não está dentro dos paramentros");
+        }
+        LocalDate dataAtual = LocalDate.now();
+        if (dataAdmissao.isBefore(dataAtual)){
+            throw new IllegalArgumentException("Data invalida.");
+        }
+        if(dataDesligamento.isBefore(dataAtual) || dataDesligamento.isAfter(dataAdmissao)){
+            throw new IllegalArgumentException("Data invalida.");
+        }
+
     }
 
-    public double calcularSalarioLiquido(){
-        return 1.0;
+    public double calcularSalarioLiquido(double salarioBruto, double percentualImpostos){
+        double valor = salarioBruto-percentualImpostos
+        return valor;
     }
 }
--- src/muitobommuitobom/Vendedor.java
+++ src/muitobommuitobom/Vendedor.java
@@ -4,11 +4,18 @@ public class Vendedor extends Empregado{
     double valorMinimoBonus;
     double percentualPremiacao;
     private double vendasMes;
+    private double vendasUltimoMes;
     private double vendasPenultimoMes;
     private boolean informouVendasMes;
 
-    public Vendedor(String nome, muitobommuitobom.TipoPessoa tipoPessoa, String documento, String endereco, String complementoEndereco, String bairro, String CEP, String cidade, muitobommuitobom.Estado estado, String telefone) {
+    public Vendedor(String nome, muitobommuitobom.TipoPessoa tipoPessoa, String documento, String endereco, String complementoEndereco, String bairro, String CEP, String cidade, muitobommuitobom.Estado estado, String telefone, double valorMinimoBonus, double percentualPremiacao) {
         super(nome, tipoPessoa, documento, endereco, complementoEndereco, bairro, CEP, cidade, estado, telefone);
+        this.informouVendasMes = false;
+        this.vendasMes = 0;
+        this.vendasPenultimoMes = 0;
+        this.vendasUltimoMes = 0;
+        this.valorMinimoBonus = valorMinimoBonus;
+        this.percentualPremiacao = percentualPremiacao;
     }
 
     void informarVendasDoMes(double valor){
